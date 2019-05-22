e15188862300b1b3f610a69782f63bcde681c35d
congpc <congpc@tctav.com>
2019-05-22 09:48:20 +0700

    Updates

---
+++ precache-manifest.e5b49b6d45e8ef724bd5b7546e07ae2e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/congpc/paper-dashboard-react/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "b70cca317b9c5c9049a2",
+    "url": "/congpc/paper-dashboard-react/static/css/main.6525f81c.chunk.css"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/congpc/paper-dashboard-react/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "0d2e61238e9ea4f6086e",
+    "url": "/congpc/paper-dashboard-react/static/js/1.0d2e6123.chunk.js"
+  },
+  {
+    "revision": "a2cf080a46a80969f633",
+    "url": "/congpc/paper-dashboard-react/static/js/runtime~main.a2cf080a.js"
+  },
+  {
+    "revision": "a6f24e7dd5470a8bd2f48ebb53dbe8d5",
+    "url": "/congpc/paper-dashboard-react/static/media/damir-bosnjak.a6f24e7d.jpg"
+  },
+  {
+    "revision": "aab414f7b69d0d4ad897e75e735f240a",
+    "url": "/congpc/paper-dashboard-react/static/media/mike.aab414f7.jpg"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/congpc/paper-dashboard-react/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "b70cca317b9c5c9049a2",
+    "url": "/congpc/paper-dashboard-react/static/js/main.b70cca31.chunk.js"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/congpc/paper-dashboard-react/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/congpc/paper-dashboard-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/congpc/paper-dashboard-react/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/congpc/paper-dashboard-react/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/congpc/paper-dashboard-react/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "0d2e61238e9ea4f6086e",
+    "url": "/congpc/paper-dashboard-react/static/css/1.1cc4c020.chunk.css"
+  },
+  {
+    "revision": "636b42fc5294dd961b59bdfc7ea1c938",
+    "url": "/congpc/paper-dashboard-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/paper-dashboard-react/precache-manifest.a888b221a5b210d947ed22e7507ca263.js"
+  "/congpc/paper-dashboard-react/precache-manifest.e5b49b6d45e8ef724bd5b7546e07ae2e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/paper-dashboard-react/index.html", {
+workbox.routing.registerNavigationRoute("/congpc/paper-dashboard-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
